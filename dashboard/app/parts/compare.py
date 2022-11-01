d691d84653df5f09c2ad493442b6a343962e26e9
Roland <62351628+admantiumblack@users.noreply.github.com>
2022-11-01 17:26:10 +0700

    change: change how layout is rendered to fix spacing

--- dashboard/app/parts/compare.py
+++ dashboard/app/parts/compare.py
@@ -16,18 +16,21 @@ def compare_stats():
     talent_names.append(talent_name_menu(st.sidebar, key='name1'))
     talent_names.append(talent_name_menu(st.sidebar, key='name2'))
     dates = date_menu(st, talent_names, 'talent_compare_dates')
-    tweet_datas = [get_tweet_fact_table(i, dates).mean() for i in talent_names]
-    stream_datas = [get_stream_fact_table(i, dates).mean() for i in talent_names]
-    cols = st.columns(2)
-    for idx, i in enumerate(cols):
-        i.title(talent_names[idx])
-        vtuber_image(i, talent_names[idx])
-        keys = ['density', 'centrality', 'avg_clustering_coef', 'reciprocity']
-        for key in keys:
-            theme = get_theme(tweet_datas[idx], tweet_datas[-(idx + 1)], key = key)
-            stat_card(i, tweet_datas[idx], key, theme=theme, component_key=f'{idx}')
+    if len(dates) == 2:
+        tweet_datas = [get_tweet_fact_table(i, dates).mean().fillna(0.0) for i in talent_names]
+        stream_datas = [get_stream_fact_table(i, dates).mean().fillna(0.0) for i in talent_names]
+        outer_cols = st.columns(2)
+        for idx, i in enumerate(outer_cols):
+            i.title(talent_names[idx])
+            vtuber_image(i, talent_names[idx])
+        for key in ['density', 'centrality', 'avg_clustering_coef', 'reciprocity']:
+            cols = st.container().columns(2)
+            for idx, j in enumerate(cols):
+                theme = get_theme(tweet_datas[idx], tweet_datas[-(idx + 1)], key = key)
+                stat_card(j, tweet_datas[idx], key, theme=theme, component_key=f'{idx}')
         
-        stream_keys = ['revenue_total', 'superchat_count']
-        for key in stream_keys:
-            theme = get_theme(stream_datas[idx], stream_datas[-(idx + 1)], key = key)
-            stat_card(i, stream_datas[idx], key, theme=theme, component_key=f'{idx}')
+        for key in ['revenue_total', 'superchat_count']:
+            cols = st.container().columns(2)
+            for idx, j in enumerate(cols):
+                theme = get_theme(stream_datas[idx], stream_datas[-(idx + 1)], key = key)
+                stat_card(j, stream_datas[idx], key, theme=theme, component_key=f'{idx}')
